ab958ccab2ee3af50adf0ee02a3bd2f49078c23e
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2023-03-01 21:55:51 -0700

    Added bootstrap, updated pages & index.js

---
+++ my-portfolio/src/components/NavTabs.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#About"
+          onClick={() => handlePageChange('About')}
+          
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#projects"
+          onClick={() => handlePageChange('Projects')}
+          
+          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('Resume')}
+         
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;

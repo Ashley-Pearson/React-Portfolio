b234a0f05b244b108cf59500fc175957ae490b79
Ashley Pearson <ashleypearson@Ashleys-MacBook-Air.local>
2023-03-02 09:13:58 -0700

    added footer, updated styling, added footer icons

--- my-portfolio/src/components/PortfolioContainer.js
+++ my-portfolio/src/components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+import Footer from './footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -27,6 +28,7 @@ export default function PortfolioContainer() {
     <div>
       
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Footer currentPage ={currentPage} handlePageChange={handlePageChange} />
       
       {renderPage()}
     </div>
---
+++ my-portfolio/src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const styles = {
+    footerIcon: {
+        background: 'white',
+        margin: '20px',
+        borderRadius: '10px',
+        width: '70px'
+    }
+}
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <a href='https://www.linkedin.com/in/ashley-pearson-578960195/'>
+            <img src='LI-In-Bug.png' alt='LinkedIn'style={styles.footerIcon}></img>
+            </a>
+            <a href='https://github.com/Ashley-Pearson'>
+                
+            </a>
+        </div>
+    )
+}

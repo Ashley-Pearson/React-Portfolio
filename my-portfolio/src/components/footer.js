860f2fa4a8cc57a001ca1685c1ecc5037a557d22
Ashley Pearson <ashley.pearson2202@gmail.com>
2023-05-10 22:22:37 -0700

    add react linkedIn icon

--- my-portfolio/src/components/footer.js
+++ my-portfolio/src/components/footer.js
@@ -1,24 +1,22 @@
 import React from 'react'
+import { RiLinkedinBoxFill } from "react-icons/ri"
 
 
-const styles = {
-    footerIcon: {
-        background: 'white',
-        margin: '20px',
-        borderRadius: '10px',
-        width: '70px'
-    }
-}
+//const styles = {
+//  footerIcon: {
+//     background: 'white',
+//    margin: '20px',
+//   borderRadius: '10px',
+//  width: '70px'
+// }
+//}
 
 export default function Footer() {
     return (
         <div className='footer'>
-            <a href='https://www.linkedin.com/in/ashley-pearson-578960195/'>
-            <img src='Linkedin_logo_PNG.png' alt='LinkedIn'style={styles.footerIcon}></img>
-            </a>
-            <a href='https://github.com/Ashley-Pearson'>
-                
+            <a href="linkedin.com">
+                <div> <RiLinkedinBoxFill /> </div>
             </a>
         </div>
-    )
+    );
 }

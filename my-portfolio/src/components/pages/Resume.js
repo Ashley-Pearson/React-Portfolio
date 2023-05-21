199c931fc2be4500a4b36fcfb1aa6706349172bc
Ashley Pearson <ashley.pearson2202@gmail.com>
2023-05-20 22:17:20 -0700

    center text

--- my-portfolio/src/components/pages/Resume.js
+++ my-portfolio/src/components/pages/Resume.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
-      <h1>Resume </h1>
+      <center><h1>Resume </h1>
       <p>
         Resume stuff - this is my resume things
-      </p>
+      </p></center>
     </div>
   );
 }

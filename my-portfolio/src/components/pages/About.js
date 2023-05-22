8eaea7659fcef709c5d8b02e360ad4682769039e
Ashley Pearson <ashley.pearson2202@gmail.com>
2023-05-21 23:01:01 -0700

    add profile pic

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 
+
 export default function About() {
   return (
     <div className="mt-32 mx-3">
       {/* Portfolio Picture */}
-      <figure>
-        <img src={"/images/Profile_pic.jpg"}
-          className="w-80 border-4 border-quaternary mx-auto my-2"
-        ></img>
-
-      </figure>
+      <div>
+                <img
+                    src="/images/Profile_Pic_1.jpg"
+                    alt="profile"
+                    className="w-80 border-4 border-quaternary mx-auto my-2"
+                />
+            </div>
       {/* About Me */}
       <div className="text-center my-3">
         <h2>My Portfoilio</h2>

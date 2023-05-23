bd1ec4abd6cea09e2971a07193a93b541d3e3acf
Ashley Pearson <ashley.pearson2202@gmail.com>
2023-05-22 21:10:26 -0700

    update About.js

--- my-portfolio/src/components/pages/About.js
+++ my-portfolio/src/components/pages/About.js
@@ -17,7 +17,7 @@ export default function About() {
       <div className="text-center my-3">
         <h2>My Portfoilio</h2>
         <h2>
-          Talk about yourself. I am a Jr Developer
+          Talk about yourself. I am a Jr Developer with a passion
         </h2>
       </div>
     </div>

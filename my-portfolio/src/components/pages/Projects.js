655a05da868c2454146d84b10e938ad3e386d061
Ashley Pearson <ashley.pearson2202@gmail.com>
2023-05-27 20:21:38 -0700

    Add orderlist of projects

--- my-portfolio/src/components/pages/Projects.js
+++ my-portfolio/src/components/pages/Projects.js
@@ -6,6 +6,11 @@ export default function Projects() {
       <center><h1>Projects</h1>
       <p>
         Add Projects here.
+        <ul>
+          <li> Flix N'Chill</li>
+          <li> Aptly Maintenance Portal </li>
+          <li> Road Sage </li>
+        </ul>
       </p></center>
     </div>
   );
